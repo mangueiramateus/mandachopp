a3fb8b871ade37900315211bc3869105903e4cb5
mangueiramateus <mateus.lima.mangueira@gmail.com>
2018-06-18 23:14:49 -0400

    Comandos ACK, ECHO

--- README.md
+++ README.md
@@ -12,14 +12,30 @@ Atividades:
 	>> VALVULA -> definição como saída (pinMode) e desativação (digitalWrite)
 2. COMUNICAÇÃO BLUETOOTH
     > Verificar se a comunicação é bem estabelecida para troca de informações
-3. PROTOCOLO DE COMUNICAÇÃO
-4. COMANDO ACK
+3. PROTOCOLO DE COMUNICAÇÃO - DONE IN 18/JUN
+    > Protocolo de comunicação definido como os seguintes comandos:
+	ACK ->		TAP1:0:0: => TAP1:OK
+	ECHO ->		TAP1:1:0: => devolve última resposta de comando
+	READ -> 	TAP1:2:0: => TAP1:2:TAG:BTN:OK = ler status do RFID e do botão
+	SENSOR ->	TAP1:3:0: => TAP1:3:OK = desativa interrupção do sensor
+			TAP1:3:1: => TAP1:3:OK = ativa interrupção do sensor
+			TAP1:3:2: => TAP1:3:PULSOS:VAZAO:OK = ler status do sensor
+	VALVES ->	TAP1:4:0: => TAP1:4:OK = ativa válvula
+			TAP1:4:1: => TAP1:4:OK = desativa válvula 
+	FILL -> 	TAP1:5:0: => TAP1:5:OK = desativa modo consumo
+			TAP1:5:PULSOS: => TAP1:5:OK = ativa modo consumo até PULSOS máximo
+	LEDON -> 	TAP1:6:LED?: => TAP1:6:OK = desativa o LED de acordo com o número:
+						0-vermelho, 1-amarelo, 2-azul, 3-verde
+	LEDOFF -> 	TAP1:7:LED: => TAP1:7:OK = ativa o LED de acordo com o número acima
+	BUZZER ->	TAP1:8:N: => TAP1:8:OK = apita o BUZZER N vezes
+	RESET ->	TAP1:9:0: => TAP1:9:OK = reseta o módulo
+4. COMANDOS ACK/ECHO - DONE IN 18/JUN
 5. COMANDO READ
 6. COMANDO SENSOR
 7. COMANDO VALVE
-8. COMANDO MODO
-9. COMANDO RESET
-10. ERROS
+8. COMANDO FILL
+9. COMANDOS LEDON, LEDOFF, BUZZER
+10. COMANDO RESET
 
 
 APP TABLET ANDROID
--- TAPv2/moduleTAP/ModuleTap/src/main.cpp
+++ TAPv2/moduleTAP/ModuleTap/src/main.cpp
@@ -1,7 +1,19 @@
+/*
+  FIRMARE 2.0 MODULE TAP MANDA CHOPP
+    Versão para segunda versão do sistema na qual este código funcionará como firmware para o arduino que 
+  por sua vez funcionará como periférico do tablet. Este último será o controlador principal.
+  
+  FAZER
+    1. Enum com os comandos e os erros
+    2. Biblioteca para cada operação
+    3. Leitura do RFID
+    4. Leitura do sensor
+
+*/
+
 #include <Arduino.h>
 #include <TimerOne.h>
 #include <SoftwareSerial.h>
-#include <SPI.h>
 #include <MFRC522.h>
 
 //INTERFACE
@@ -10,8 +22,8 @@
 #define AZUL 16
 #define VERDE 17
 
-#define BUZZER 18
-#define BTN 21
+#define BUZZER 5
+#define BTN 20
 
 //CONTROLE DO FLUXO
 #define SENSOR 2
@@ -32,6 +44,7 @@ SoftwareSerial blue(3, 4);
 #define LEDon(led) digitalWrite(led, HIGH)
 #define LEDoff(led) digitalWrite(led, LOW)
 String ler_TAG();
+char ler_BTN();
 void apitar(byte vezes, byte tempo);
 
 void setup(){
@@ -61,28 +74,73 @@ void setup(){
 }
 
 bool isData = false;
-String data = "";
-String tag = "", btn = "0";
+byte cmd = 0;
+unsigned long int param = 0;
+String data = "", ans = "";
+String tag = "";
+char btn = '0';
 void loop(){
+
+  //Verifica se há comando do bluetooth
   while (blue.available()){
     data += (char)blue.read();
     isData = true;
     delay(1);
   }
+
+  //Verifica se TAG ou bortão já foi lida pelo bluetooth. Se não faz nova leitura
   if (tag == "") tag = ler_TAG();
-  //if (btn != "1") btn = ler_BTN();
+  if (btn == '0') btn = ler_BTN();
+
+  //Há novo dado
   if (isData){
+    //Espelha o dado
     Serial.println(data);
+    //Verifica o cabeçalho
     if (data.indexOf("TAP1") != -1){
-      if (data.indexOf("ACK") != -1) blue.print("TAP1:OK");
-      if (data.indexOf("READ") != -1) {
-        if (tag == "") tag = "__NULL__";
-        else Serial.println(tag);
-        blue.print("TAP1:" + tag + ":" + btn);
-        Serial.println("TAP1:" + tag + ":" + btn);
-        tag = "";
-        btn = "0";
+      byte aux1 = data.indexOf(":");
+      byte aux2 = data.indexOf(":", aux1+1);
+      cmd = data.substring(aux1+1, aux2).toInt();
+      aux1 = data.indexOf(":", aux2+1);
+      param = data.substring(aux2+1, aux1).toInt();
+      bool OK = data.substring(aux1+1, aux1+3) == "OK";
+      bool write = true;
+      if (OK){
+        //Comando ACK
+        if (cmd == 0)  ans = "TAP1:OK";
+        //Comando ECHO
+        else if (cmd == 1) {
+          if (ans == "") ans = "TAP1:OK";
+        }
+        //Comando READ
+        else if (cmd == 2) {
+          if (tag == "") tag = "__NULL__";
+          else Serial.println(tag);
+          ans = "TAP1:2:" + tag + ":" + btn + ":OK";
+          tag = "";
+          btn = '0';
+        }
+        //Comando SENSOR
+        else if (cmd == 3) ans = "TAP1:3:OK";
+        //Comando VALVE
+        else if (cmd == 4) ans = "TAP1:4:OK";
+        //Comando FILL
+        else if (cmd == 5) ans = "TAP1:5:OK";
+        //Comando LEDON
+        else if (cmd == 6) ans = "TAP1:6:OK";
+        //Comando LEDOFF
+        else if (cmd == 7) ans = "TAP1:7:OK";
+        //Comando BUZZER
+        else if (cmd == 8) ans = "TAP1:8:OK";
+        //Comando RESET
+        else if (cmd == 9) ans = "TAP1:9:OK";
+        else write = false;
+        if (write){
+          blue.println(ans);
+          Serial.println(ans);
+        }
       }
+      else blue.println("ERROR");  
     }
     else blue.println("ERROR");
     data = "";
@@ -108,7 +166,7 @@ void calculo() {
 
 String ler_TAG() {
   if (RFID.PICC_IsNewCardPresent() && RFID.PICC_ReadCardSerial()) {
-
+    apitar(2, 20);
     String conteudo = "";
     for (byte i = 0; i < RFID.uid.size; i++)
     {
@@ -120,9 +178,9 @@ String ler_TAG() {
   else return "";
 }
 
-String ler_BTN(){
-  if (analogRead(BTN) < 100) return "1";
-  else return "0";
+char ler_BTN(){
+  if (analogRead(BTN) < 100) return '1';
+  else return '0';
 }
 
 void apitar(byte vezes, byte tempo) {
